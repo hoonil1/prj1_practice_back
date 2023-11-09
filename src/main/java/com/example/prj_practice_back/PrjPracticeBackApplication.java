1df6e4cd8f0b6cd2c93fd9b113f2b27be8dc43b1
hoonil1 <phi0588@naver.com>
2023-11-09 11:45:50 +0900

    최초 설정 및 boardController Post  /api/board/add 작성

---
+++ src/main/java/com/example/prj_practice_back/PrjPracticeBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.prj_practice_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjPracticeBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrjPracticeBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/prj_practice_back/PrjPracticeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prj_practice_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjPracticeBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
