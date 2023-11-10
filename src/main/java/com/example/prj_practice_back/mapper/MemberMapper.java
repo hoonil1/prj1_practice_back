7b35b56d9e7b1a0424c70382c241bf014943feef
hoonil1 <phi0588@naver.com>
2023-11-10 14:26:45 +0900

    ID 중복확인 기능 추가

--- src/main/java/com/example/prj_practice_back/mapper/MemberMapper.java
+++ src/main/java/com/example/prj_practice_back/mapper/MemberMapper.java
@@ -3,6 +3,7 @@ package com.example.prj_practice_back.mapper;
 import com.example.prj_practice_back.domain.Member;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface MemberMapper {
@@ -13,4 +14,10 @@ insert into member (id,password,email)
 values(#{id},#{password},#{email});
 """)
     int insert(Member member);
+
+    @Select("""
+            select id from member
+            where id = #{id}
+            """)
+    String selectId(String id);
 }
