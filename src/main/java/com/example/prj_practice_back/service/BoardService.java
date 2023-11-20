6b2ec30d0db71107c87fe2e684cc9b97991fdf25
hoonil1 <phi0588@naver.com>
2023-11-20 14:45:09 +0900

    파일 업로드 크기 제한 Service 트랜젝션 처리

--- src/main/java/com/example/prj_practice_back/service/BoardService.java
+++ src/main/java/com/example/prj_practice_back/service/BoardService.java
@@ -7,9 +7,14 @@ import com.example.prj_practice_back.mapper.CommentMapper;
 import com.example.prj_practice_back.mapper.FileMapper;
 import com.example.prj_practice_back.mapper.LikeMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.ObjectCannedACL;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import java.io.File;
 import java.io.IOException;
@@ -25,6 +30,10 @@ public class BoardService {
     private final CommentMapper commentMapper;
     private final LikeMapper likeMapper;
     private final FileMapper fileMapper;
+    @Value(("#{aw3.s3.bucket.name")
+    private final String bucket;
+    )
+    private final S3Client s3;
 
     public boolean save(Board board, MultipartFile[] files, Member login) throws IOException {
         //
@@ -49,16 +58,30 @@ public class BoardService {
     }
 
     private void upload(Integer boardId, MultipartFile file) throws IOException {
-        // 파일 저장 경로
-        // C:\Temp\prj1\게시물번호\파일명
-        File folder = new File("C:\\Temp\\prj1\\" + boardId);
-        if (!folder.exists()) {
-            folder.mkdirs();
-        }
+//        // 파일 저장 경로
+//        // C:\Temp\prj1\게시물번호\파일명
+//        File folder = new File("C:\\Temp\\prj1\\" + boardId);
+//        if (!folder.exists()) {
+//            folder.mkdirs();
+//        } ******* 로컬에 저장할때 코드 ******
+//        String path = folder.getAbsolutePath() + "\\" + file.getOriginalFilename();
+//        File des = new File(path);
+//        file.transferTo(des);
 
-        String path = folder.getAbsolutePath() + "\\" + file.getOriginalFilename();
-        File des = new File(path);
-        file.transferTo(des);
+        // ******* S3 저장 **************
+        String key = "prj1/" + boardId + "/" + file.getOriginalFilename();
+
+
+        PutObjectRequest objectRequest = PutObjectRequest.builder()
+                .bucket(bucket)
+                .key(key)
+                .acl(ObjectCannedACL.PUBLIC_READ)
+                .build();
+
+
+
+
+        s3.putObject(objectRequest, RequestBody.fromInputStream(file.getInputStream(),file.getSize()));
 
 
     }
