8d3475c3a6ca11ef67f287eb7912b969565e91ed
hoonil1 <phi0588@naver.com>
2023-11-21 16:45:45 +0900

    제목, 본문별 검색

--- src/main/java/com/example/prj_practice_back/controller/BoardController.java
+++ src/main/java/com/example/prj_practice_back/controller/BoardController.java
@@ -1,14 +1,19 @@
 package com.example.prj_practice_back.controller;
 
+
 import com.example.prj_practice_back.domain.Board;
 import com.example.prj_practice_back.domain.Member;
 import com.example.prj_practice_back.service.BoardService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,8 +23,9 @@ public class BoardController {
     private final BoardService service;
 
     @PostMapping("add")
-    public ResponseEntity add(@RequestBody Board board,
-                              @SessionAttribute(value = "login", required = false) Member login) {
+    public ResponseEntity add(Board board,
+                              @RequestParam(value = "uploadFiles[]", required = false) MultipartFile[] files,
+                              @SessionAttribute(value = "login", required = false) Member login) throws IOException {
 
         if (login == null) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
@@ -29,16 +35,24 @@ public class BoardController {
             return ResponseEntity.badRequest().build();
         }
 
-        if (service.save(board, login)) {
+        if (service.save(board, files, login)) {
             return ResponseEntity.ok().build();
         } else {
             return ResponseEntity.internalServerError().build();
         }
     }
 
+    // /api/board/list?p=6
+    // /api/board/list?k=java
+    // /api/board/list?p=7&k=java
     @GetMapping("list")
-    public List<Board> list() {
-        return service.list();
+    public Map<String, Object> list(
+            @RequestParam(value = "p", defaultValue = "1") Integer page,
+            @RequestParam(value = "k", defaultValue = "") String keyword,
+            @RequestParam(value = "c", defaultValue = "all") String category) {
+
+
+        return service.list(page, keyword, category);
     }
 
     @GetMapping("id/{id}")
@@ -65,8 +79,11 @@ public class BoardController {
     }
 
     @PutMapping("edit")
-    public ResponseEntity edit(@RequestBody Board board,
-                               @SessionAttribute(value = "login", required = false) Member login) {
+    public ResponseEntity edit(Board board,
+                               @RequestParam(value = "removeFileIds[]", required = false) List<Integer> removeFileIds,
+                               @RequestParam(value = "uploadFiles[]", required = false) MultipartFile[] uploadFiles,
+                               @SessionAttribute(value = "login", required = false) Member login) throws IOException {
+
         if (login == null) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build(); // 401
         }
@@ -76,7 +93,7 @@ public class BoardController {
         }
 
         if (service.validate(board)) {
-            if (service.update(board)) {
+            if (service.update(board, removeFileIds, uploadFiles)) {
                 return ResponseEntity.ok().build();
             } else {
                 return ResponseEntity.internalServerError().build();
